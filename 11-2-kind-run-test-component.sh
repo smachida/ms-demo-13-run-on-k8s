0137c2ff3437646508c6c523a745284c35052c0e
Shuichi Machida <smachida@macbook.local>
2022-06-10 14:55:56 +0900

    updated

--- 11-2-kind-run-test-component.sh
+++ 11-2-kind-run-test-component.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-HOST=172.19.255.200 PORT=443 ./scripts/01-test-em-all.sh
+HOST=172.18.255.200 PORT=443 ./scripts/01-test-em-all.sh
+#HOST=localhost PORT=30443 ./scripts/01-test-em-all.sh
+
--- scripts/22-3-tce-run-load-generator.sh
+++ scripts/22-3-tce-run-load-generator.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-siege https://172.19.255.200:443/actuator/health -c1 -d1 -v
+siege https://172.18.255.200:443/actuator/health -c1 -d1 -v
+#siege https://localhost:30443/actuator/health -c1 -d1 -v
+
